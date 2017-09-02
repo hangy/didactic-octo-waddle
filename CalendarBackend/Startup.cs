b29a3593d3d458f8dae0c5e3147ff08d05337cf3
hangy <hangy@hangy.de>
2017-09-02 15:29:02 +0200

    Add and initialize MediatR for DI

--- CalendarBackend/Startup.cs
+++ CalendarBackend/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MediatR;
 
 namespace CalendarBackend
 {
@@ -22,6 +23,7 @@ namespace CalendarBackend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddMediatR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
