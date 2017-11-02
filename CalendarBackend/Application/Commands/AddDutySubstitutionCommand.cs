9e27b926991b4344d0d31a42110dc4e71b9ee170
hangy <hangy@hangy.de>
2017-11-02 12:04:23 +0100

    Add duty commands and controller.

---
+++ CalendarBackend/Application/Commands/AddDutySubstitutionCommand.cs
@@ -0,0 +1,22 @@
+﻿namespace CalendarBackend.Application.Commands
+{
+    using MediatR;
+    using NodaTime;
+    using System;
+
+    public class AddDutySubstitutionCommand : IRequest
+    {
+        public AddDutySubstitutionCommand(Guid dutyId, string userId, DateInterval interval)
+        {
+            this.DutyId = dutyId;
+            this.UserId = userId;
+            this.Interval = interval;
+        }
+
+        public Guid DutyId { get; }
+
+        public DateInterval Interval { get; }
+
+        public string UserId { get; }
+    }
+}
---
+++ CalendarBackend/Application/Commands/AddUserToDutyCommand.cs
@@ -0,0 +1,22 @@
+﻿namespace CalendarBackend.Application.Commands
+{
+    using MediatR;
+    using NodaTime;
+    using System;
+
+    public class AddUserToDutyCommand : IRequest
+    {
+        public AddUserToDutyCommand(Guid dutyId, string userId, LocalDate start)
+        {
+            this.DutyId = dutyId;
+            this.UserId = userId;
+            this.Start = start;
+        }
+
+        public Guid DutyId { get; }
+
+        public LocalDate Start { get; }
+
+        public string UserId { get; }
+    }
+}
