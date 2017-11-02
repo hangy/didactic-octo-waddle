0b15eb3cf9fd255e0712b2e624428be8544f8de7
hangy <hangy@hangy.de>
2017-11-02 12:35:54 +0100

    Added on-duty query handlers.

---
+++ CalendarBackend/Application/QueryHandlers/GetConcreteDutyEntryHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace CalendarBackend.Application.QueryHandlers
+{
+    using CalendarBackend.Application.Queries;
+    using CalendarBackend.Infrastructure.ReadModel;
+    using MediatR;
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class GetConcreteDutyEntryHandler : ICancellableAsyncRequestHandler<GetConcreteDutyEntry, Duty>
+    {
+        private readonly DutyReadModel model;
+
+        public GetConcreteDutyEntryHandler(DutyReadModel model)
+        {
+            this.model = model ?? throw new ArgumentNullException(nameof(model));
+        }
+
+        public async Task<Duty> Handle(GetConcreteDutyEntry message, CancellationToken cancellationToken)
+        {
+            var entries = await this.model.GetEntriesAsync(cancellationToken).ConfigureAwait(false);
+            cancellationToken.ThrowIfCancellationRequested();
+            return entries.SingleOrDefault(e => e.Id == message.Id);
+        }
+    }
+}
